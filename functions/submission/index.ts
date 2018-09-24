8a312b12009c03d0bf853cf59c0ca71714e4ac74
Jamie Magee <jamie.magee@gmail.com>
2018-09-24 23:12:45 +0200

    Migrate to sendgrid bindings

--- functions/submission/index.ts
+++ functions/submission/index.ts
@@ -1,25 +1,47 @@
-import {
-  Context,
-  HttpRequest,
-  HttpResponse,
-  HttpStatusCode
-} from 'azure-functions-ts-essentials';
+import { Context, HttpRequest } from 'azure-functions-ts-essentials';
+import { MailJSON } from '@sendgrid/helpers/classes/mail';
 
 import { getSubmission, parseUrl } from '../shared/reddit-utilities';
 import { handleGenericError } from '../shared/function-utilities';
 import { comments } from '../shared/template-utilities';
-import { sendMail } from '../shared/email-utilities';
 import { Submission } from '../shared/reddit-utilities/submission';
-import { SentMessageInfo } from 'nodemailer';
 
-export async function run(context: Context, req: HttpRequest): Promise<void> {
+export async function run(
+  context: Context,
+  req: HttpRequest,
+  message: MailJSON
+): Promise<MailJSON> {
   try {
     const id: string = parseUrl(req.body);
 
     const submission: Submission = await getSubmission(id);
     const html: string = await comments(submission);
-    await sendMail(submission, html);
-
+    return {
+      ...message,
+      personalizations: [
+        {
+          to: [
+            {
+              email: 'jamie.magee@gmail.com'
+            }
+          ]
+        }
+      ],
+      subject: submission.title,
+      attachments: [
+        {
+          content: Buffer.from(html).toString('base64'),
+          filename: submission.title + '.html',
+          type: 'text/html'
+        }
+      ],
+      content: [
+        {
+          type: 'text/plain',
+          value: ' '
+        }
+      ]
+    };
   } catch (e) {
     context.log.error(e);
     handleGenericError(context, '');
